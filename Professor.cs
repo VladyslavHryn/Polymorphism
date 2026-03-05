1d26529f96de272c9278181cb8d2c0d7e757c4e8
Vladyslav Hryn <vladyslav.hryn.25@kse.og.ua>
2026-03-05 22:09:07 +0200

    professor class

---
+++ Professor.cs
@@ -0,0 +1,16 @@
+﻿public class Professor : UniversityMemeber
+{
+    public Professor(string name, string memberId) : base(name, memberId)
+    {
+        public override void PerformDuties()
+            base.PerformDuties();
+        ActionLog.Add("Lecture delivered");
+        Console.WriteLine($"{Name} delivered a lecture (actions today: {ActionLog.Count})")
+    }
+
+public void ConductResearch(string topic)
+{
+    Console.WriteLine($"{Name} is now researching: {topic}");
+}
+
+}
