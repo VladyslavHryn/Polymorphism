8ce2250f80592fd886aeb499dbf0421ff26afc6c
Vladyslav Hryn <vladyslav.hryn.25@kse.og.ua>
2026-03-05 22:03:44 +0200

    uni member class

---
+++ UniversityMember.cs
@@ -0,0 +1,30 @@
+﻿public class UniversityMember
+{
+    private string _name;
+
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new Exception("Name cannot be empty!");
+            _name = value;
+        }
+    }
+
+    public string MemberId { get; }
+    protected List<string> ActionLog = new List<string>();
+
+    public UniversityMember(string name, string memberId)
+    {
+        Name = name;
+        MemberId = memberId;
+    }
+
+    public virtual void PerformDuties()
+    {
+        if (ActionLog.Count >= 5)
+            throw new Exception($"{Name} already hit the 5 action daily limit!");
+    }
+}
